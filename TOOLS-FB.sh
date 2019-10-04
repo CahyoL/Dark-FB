d0affcd5ff5f453b11c83e7241de56b68afa507c
TERNUX <54984275+CahyoL@users.noreply.github.com>
2019-10-04 11:29:35 +0700

    Add files via upload

---
+++ TOOLS-FB.sh
@@ -0,0 +1,272 @@
+clear
+echo
+echo
+echo
+clear
+echo
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+clear
+echo $me"PERHATIAN!!"
+echo $ku"Ketik y jika belum menginstall bahan"
+echo $ku"Ketik n jika sudah menginstall bahan"
+echo
+echo $ku"Apakah kamu ingin menginstall bahan (y/n?)"
+read conf
+
+if [ $conf = y ]
+then
+clear
+pkg install git
+pkg install python2
+pkg install python
+pkg install php
+pkg install ruby
+pkg install figlet
+pkg install bash
+pip2 install mechanize
+pip2 install requests
+pkg install lolcat
+clear
+fi
+if [ $conf = n ]
+then
+clear
+fi
+clear
+echo
+echo $cy"SUBCRIBE CHANNEL : CL.GAMING"
+echo $cy"LINK CHANNEL : None"
+echo $cy"WA : 081365443940"
+echo $cy"PLEASE SUBSCRIBE"
+sleep 1
+echo $i"UPDATE : 29 September  2019"
+echo $i"PENAMBAHAN TOOLS MENJADI 31"
+echo $i"DAN SCRIPT INI DI ENCRYPT"
+echo
+sleep 5
+
+figlet selamat | lolcat
+figlet datang | lolcat
+sleep 2
+clear
+echo $pur "
+===================================="
+echo $cy "
+          _/﹋\_
+          (ಠ益ಠ)
+          <,︻╦╤─ - - - MATI LOH ASW
+          _/﹋\_"
+echo $ku "
+█████████
+\033[1;93m█▄█████▄█      \033[1;91m●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+\033[1;93m█$me▼▼▼▼▼ \033[1;92m- _ --_--\033[1;95m╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗ 
+\033[1;93m█ \033[1;92m \033[1;92m_-_-- -_ --__\033[1;93m ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
+\033[1;93m█$me▲▲▲▲▲\033[1;92m--  - _ --\033[1;96m═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝ \033[1;96mtools-Dark
+\033[1;93m█████████      \033[1;92m«----------✧----------»
+\033[1;93m ██ ██
+\033[1;93m╔════════════════════════════════════════════╗
+\033[1;93m║\033[1;96m* \033[1;93mAuthor  \033[1;93m: \033[1;93mBrother•|Mr.TuanTools   \033[1;93m        ║
+\033[1;93m║\033[1;96m* \033[1;93mGitHub  \033[1;93m: \033[1;93m\033[4mhttps://github.com/CahyoL\033[0m \033[1;93m      ║
+\033[1;93m╚════════════════════════════════════════════╝"""
+echo $ku "
+===================================="
+sleep 2
+
+echo $cy "
+┏━━━━━┫{menu dark fb}┣━━━━━┓
+┃
+┠──[0] untuk instal bahan agar work ✔
+┃
+┠──[1] Dark 1.6 ✔
+┃
+┠──[2] Dark 1.7 [-FREE-] ✔
+┃
+┠──[3] Dark 1.8 ✔
+┃
+┠──[4] Dark premium ✔
+┃
+┠──[5] Dark VIP ✔
+┃
+┠──[6] Dark pro ✔
+┃
+┠──[7] Dark Diamond ✔
+┃
+┠──[8] Dark v.1.2 ✔
+┠
+┠──[9] v2.Dark ✔
+┠
+┠──[10] Dark-Cyber11 ✔
+┠
+┠──[11] cyber 2.5 ✔
+┠
+┠──[12] IBLI2
+┠
+┠──[13] Dark-Asu
+┠
+┠──[14] BHV26
+┠
+┠──[15] MR.Rendy-Dark
+┠
+┗────[99] exit ✘"
+echo "\033[35;1m"
+read -p "[Pilih Nomor]> " bro
+if [ $bro = 0 ] || [ $bro]
+then
+clear
+pkg install ruby
+gem install lolcat
+pip2 install requests
+pip2 install mechanize
+pip2 install requirements
+sh yasin.sh
+fi
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+pkg install git
+git clone  https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+pkg install git
+git clone https://github.com/muhalvin/darkfb7
+cd darkfb7
+python2 dark.py
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+pkg install git
+git clone https://github.com/Mr-XsZ/Dark-Fb
+cd Dark-Fb
+python2 dark.py
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+pkg install git
+git clone https://github.com/TheMagizz/DarkPremium
+cd DarkPremium
+python2 DarkFB.py
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+pkg install git
+git clone https://github.com/m4rche3ll-cyber/dark-vpro
+cd dark-vpro
+python2 dark-vpro.py
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+git clone https://github.com/wira2611/Pro2611
+cd Pro2611
+python2 Pro.py
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+git clone https://github.com/wira2611/diamon
+ls
+cd diamon
+ls
+python2 diamond-1.py
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+git clone https://github.com/cyber2611/v1.2
+ls
+cd v1.2
+ls
+python2 Dragon-v1.2.py
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+git clone https://github.com/cyber2611/v2-dark
+cd v2.dark
+ls
+python2 v2.dark.py
+if
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+git clone https://github.com/cyber2611/dark-cyber11
+cd dark-cyber11
+ls
+python2 kapten.py
+if
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+git clone https://github.com/cyber2611/cyber2.5
+cd cyber2.5
+ls
+python2 Premium2.5.py
+if
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+git clone https://github.com/cyber2611/IBLI2
+cd IBLI2
+ls
+python2 dark-iblis.py
+if
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+git clone https://github.com/cyber2611/dark-asu
+cd dark-asu
+ls
+python2 kimak.py
+if
+
+if if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+git clone https://github.com/cyber2611/BHV26
+cd BHV26
+ls
+python2 vip-1.9.py
+if
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+git clone https://github.com/cyber2611/Mr.RENDY
+cd Mr.RENDY
+ls
+python2 Mr.rendy.py
+if
+
+if [ $bro = 99 ] || [ $bro = 99 ]
+then
+figlet THANKS | lolcat
+sleep 1
+echo $i "thank you"
+exit
+fi
